23b2dc2860a3a8eb1356d207c1f775c499a7fb18
anamilatul <anamilatulaulia@gmail.com>
2023-02-06 22:34:00 +0700

    update

--- lib/pages/form_page.dart
+++ lib/pages/form_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notes_app/utils/notes_database.dart';
+import '../models/note.dart';
 
 class FormPage extends StatefulWidget {
   const FormPage({super.key});
@@ -8,11 +10,99 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  TextEditingController titleControler = TextEditingController();
+  TextEditingController descControler = TextEditingController();
+
+  Future addNote() async {
+    final note = Note(
+      title: titleControler.text,
+      description: descControler.text,
+      time: DateTime.now(),
+    );
+    await NotesDatabase.instance.create(note);
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Form'),
+        title: const Text('Form'),
+        actions: [
+          InkWell(
+            onTap: addNote,
+            child: const Icon(Icons.save),
+          ),
+          const SizedBox(
+            width: 16,
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: ListView(children: [
+          TextFormField(
+            controller: titleControler,
+            maxLines: 1,
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 2,
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 2,
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              hintText: 'Title',
+              hintStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            controller: descControler,
+            maxLines: 16,
+            style: const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 2,
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  width: 2,
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              hintText: 'Catatan . . .',
+              hintStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ]),
       ),
     );
   }
